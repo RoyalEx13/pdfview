a305b47424c94421246e23b637d91c1f42690a10
Worachet Baramee <worachet@bananacoding.com>
2025-08-08 11:18:59 +0700

    add navbad and grid layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './shared/reusableComponent/footer/footer';
 import { sharedModule } from './core/shared/shared';
+import { Navbar } from './shared/reusableComponent/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, sharedModule],
+  imports: [sharedModule,RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
